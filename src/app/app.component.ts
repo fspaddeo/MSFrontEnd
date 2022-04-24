2c3b8a1e34132430251fe07077700499a04e268a
fspaddeo <fspaddeo@yahoo.com>
2022-04-24 16:53:36 +0200

    First component: worker list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    this.juniorWorkers =[{
+      name : 'Spiderman',
+      birth: Date.now(),
+      salary: 1000
+    },
+    {
+      name : 'Moana',
+      birth: Date.now(),
+      salary: 1500
+    },
+    {
+      name : 'Fantastic beasts',
+      birth: Date.now(),
+      salary: 1300
+    }];
+
+    this.seniorWorkers =[{
+      name : 'Spiderman',
+      birth: Date.now(),
+      salary: 4000
+    },
+    {
+      name : 'Moana',
+      birth: Date.now(),
+      salary: 3500
+    },
+    {
+      name : 'Fantastic beasts',
+      birth: Date.now(),
+      salary: 3300
+    }];
+    
+  }
+  juniorWorkers!: { name: string; birth: number; salary: number; }[];
+  seniorWorkers!: { name: string; birth: number; salary: number; }[];
   title = 'MSFrontEnd';
+  
 }
---
+++ src/app/workers/worker-list/worker-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-worker-list',
+  templateUrl: './worker-list.component.html',
+  styleUrls: ['./worker-list.component.css']
+})
+export class WorkerListComponent implements OnInit {
+
+  constructor() { }
+  
+  @Input()
+  workers!: { name: string; birth: number; salary: number; }[];
+
+  ngOnInit(): void {
+    
+  }
+
+}
